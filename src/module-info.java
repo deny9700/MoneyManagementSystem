dc95726b7e1a6521f3498eeba7ed1674068b1a06
deny9700 <deny9700@naver.com>
2021-03-22 14:12:58 +0900

    2

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,13 @@
-module MenuManager {
+module MoneyManagementSystem {
+}
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Money Spend");
+		System.out.println("2. Delete Money Spend");
+		System.out.println("3. Edit Money Spend");
+		System.out.println("4. View Money Spend");
+		System.out.println("5. Exit");
+	}
+
 }
