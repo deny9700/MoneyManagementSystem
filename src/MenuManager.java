fe33e6490c1312c7438b3bd5fa8dd77127ce0658
deny9700 <deny9700@naver.com>
2021-03-22 17:29:24 +0900

    Update MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MenuManager {
 	
 	public static void showMenu () {
-		System.out.println("*** Money Management System Menu");
+		System.out.println("*** Money Management System Menu ***");
 		System.out.println("1. Add Money Spend");
 		System.out.println("2. Delete Money Spend");
 		System.out.println("3. Edit Money Spend");
@@ -17,26 +17,36 @@ public class MenuManager {
 		int num = 0;
 		Scanner input = new Scanner(System.in);
 		
-		showMenu();
 		while (num != 6) {
+			showMenu();
 			num = input.nextInt();
-			
 			if (num == 1) {
-				
+				System.out.println("Eat:1, Transportation:2, Play outside:3, Other:4");
+				System.out.print("Type of Money Spend:");
+				int typemoneyspend = input.nextInt();
+				System.out.print("Date of Money Spend:");
+				String datemoneyspend = input.next();
+				System.out.print("Amount of Money Spend:");
+				int amountmoneyspend = input.nextInt();
+				break;
 			}
 			else if (num == 2) {
-				
+				System.out.print("Delete Money Spend:");
+				int deletemoneyspend = input.nextInt();
+				break;
 			}
 			else if (num == 3) {
-				
+				System.out.print("Edit Money Spend:");
+				int editmoneyspend = input.nextInt();
+				break;
 			}
 			else if (num == 4) {
-				
+				System.out.print("View Money Spend:");
+				int viewmoneyspend = input.nextInt();
 			}
 			else if (num == 5) {
 				showMenu();
 			}
-
 		}
 	}
 }
