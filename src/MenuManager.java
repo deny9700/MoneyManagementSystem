19b81108b376669825786a4a8fde30fcabb2639b
deny9700 <deny9700@naver.com>
2021-03-22 15:14:26 +0900

    menu update

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,10 +1,42 @@
-public class MenuManager {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class MenuManager {
+	
+	public static void showMenu () {
+		System.out.println("*** Money Management System Menu");
 		System.out.println("1. Add Money Spend");
 		System.out.println("2. Delete Money Spend");
 		System.out.println("3. Edit Money Spend");
 		System.out.println("4. View Money Spend");
-		System.out.println("5. Exit");
+		System.out.println("5. Show Menu");
+		System.out.println("6. Exit");
+		System.out.println("Select one number between 1 - 6");
+	}
+	
+	public static void main(String[] args) {
+		int num = 0;
+		Scanner input = new Scanner(System.in);
+		
+		showMenu();
+		while (num != 6) {
+			num = input.nextInt();
+			
+			if (num == 1) {
+				
+			}
+			else if (num == 2) {
+				
+			}
+			else if (num == 3) {
+				
+			}
+			else if (num == 4) {
+				
+			}
+			else if (num == 5) {
+				showMenu();
+			}
+
+		}
 	}
 }
