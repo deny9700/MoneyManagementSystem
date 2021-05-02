ce601470e58f6086465894b25cf1a17e01612a88
deny9700 <80666236+deny9700@users.noreply.github.com>
2021-05-02 19:52:22 +0900

    Update CreditCardMoneyspend.java

--- src/Moneyspend/CreditCardMoneyspend.java
+++ src/Moneyspend/CreditCardMoneyspend.java
@@ -5,30 +5,33 @@ import java.util.Scanner;
 public class CreditCardMoneyspend extends Moneyspend {
 	
 	public void getMoneyInput(Scanner input) {
-		System.out.print("정기적으로 지출되는 돈입니까?(한달):");
-		char answer = input.next().charAt(0);
-        if (answer == 'Y'||answer == 'y') {
-            System.out.print("Date of Money Spend in One Month: ");
-            String sMoneydate = input.next();
-            this.setMoneydate(sMoneydate);
-            System.out.print("Amount of Money Spend: ");
-            int sMoneyamount = input.nextInt();
-            this.setMoneyamount(sMoneyamount);
-            System.out.print("Detail of Money Spend(No Space): ");
-            input.nextLine();
-    		String sMoneydetail = input.nextLine();
-    		this.setMoneydetail(sMoneydetail);
-            break;
-        }
-        else if(answer == 'n' || answer == 'N') {
-        	this.setMoneydate(sMoneydate);
-            this.setWeekplan(weekplan);
-            this.setEdate(edate);
-            break;
-        }
-        else {
-
-        }
+		char answer= 'x';
+		while(answer !='y'&&answer!='Y'&&answer!='n'&&answer!='N') {
+			System.out.print("정기적으로 지출되는 돈입니까?(Y/N):");
+			answer = input.next().charAt(0);
+	        if (answer == 'Y'||answer == 'y') {
+	            System.out.print("Date of Money Spend in One Month: ");
+	            String sMoneydate = input.next();
+	            this.setMoneydate(sMoneydate);
+	            System.out.print("Amount of Money Spend: ");
+	            int sMoneyamount = input.nextInt();
+	            this.setMoneyamount(sMoneyamount);
+	            System.out.print("Detail of Money Spend(No Space): ");
+	            input.nextLine();
+	    		String sMoneydetail = input.nextLine();
+	    		this.setMoneydetail(sMoneydetail);
+	            break;
+	        }
+	        else if(answer == 'n' || answer == 'N') {
+	        	this.setMoneydate(moneydate);
+	            this.setMoneyamount(moneyamount);
+	            this.setMoneydetail(moneydetail);
+	            break;
+	        }
+	        else {
+	        }
+		}
+		
 		System.out.println("Eat:1, Transportation:2, Play outside:3, Other:4\n"
 				+ "+ spend number in 3 digits ex)1001,2033");
 		System.out.print("Type of Money Spend:");
