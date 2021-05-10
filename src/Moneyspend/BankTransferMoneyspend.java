ef5ca8c71f798e341603d6643ddb4fb02180e648
deny9700 <80666236+deny9700@users.noreply.github.com>
2021-05-10 19:58:46 +0900

    5/10 last modify

--- src/Moneyspend/BankTransferMoneyspend.java
+++ src/Moneyspend/BankTransferMoneyspend.java
@@ -41,7 +41,24 @@ public class BankTransferMoneyspend extends Moneyspend implements MoneyInput {
 
 	    	        else if(answer2 == 'n' || answer2 == 'N') {
 
-	    	        	super.getMoneyInput(input);
+	    	        	System.out.println("Eat:1, Transportation:2, Play outside:3, Other:4\n"
+	    	    				+ "+ spend number in 3 digits ex)1001,2033");
+	    	    		System.out.print("Type of Money Spend:");
+	    	    		int moneytype = input.nextInt();
+	    	    		this.setMoneytype(moneytype);
+	    	    		
+	    	    		System.out.print("Date of Money Spend History:");
+	    	    		String moneydate = input.next();
+	    	    		this.setMoneydate(moneydate);
+	    	    		
+	    	    		System.out.print("Amount of Money Spend History(Won):");
+	    	    		int moneyamount = input.nextInt();
+	    	    		this.setMoneyamount(moneyamount);
+	    	    		
+	    	    		System.out.print("Detail of Money Spend History(No Space):");
+	    	    		input.nextLine();
+	    	    		String moneydetail = input.nextLine();
+	    	    		this.setMoneydetail(moneydetail);
 	    	            break;
 	    	        }
 	    	        
@@ -74,7 +91,24 @@ public class BankTransferMoneyspend extends Moneyspend implements MoneyInput {
 
 	    	        else if(answer2 == 'n' || answer2 == 'N') {
 	    		        
-	    	        	super.getMoneyInput(input);
+	    	        	System.out.println("Eat:1, Transportation:2, Play outside:3, Other:4\n"
+	    	    				+ "+ spend number in 3 digits ex)1001,2033");
+	    	    		System.out.print("Type of Money Spend:");
+	    	    		int moneytype = input.nextInt();
+	    	    		this.setMoneytype(moneytype);
+	    	    		
+	    	    		System.out.print("Date of Money Spend History:");
+	    	    		String moneydate = input.next();
+	    	    		this.setMoneydate(moneydate);
+	    	    		
+	    	    		System.out.print("Amount of Money Spend History(Won):");
+	    	    		int moneyamount = input.nextInt();
+	    	    		this.setMoneyamount(moneyamount);
+	    	    		
+	    	    		System.out.print("Detail of Money Spend History(No Space):");
+	    	    		input.nextLine();
+	    	    		String moneydetail = input.nextLine();
+	    	    		this.setMoneydetail(moneydetail);
 	    	            break;
 	    	        }
 	    	        else {
--- src/Moneyspend/CreditCardMoneyspend.java
+++ src/Moneyspend/CreditCardMoneyspend.java
@@ -30,7 +30,24 @@ public class CreditCardMoneyspend extends Moneyspend implements MoneyInput {
 	        
 	        else if(answer == 'n' || answer == 'N') {
 	        	
-	        	super.getMoneyInput(input);
+	        	System.out.println("Eat:1, Transportation:2, Play outside:3, Other:4\n"
+	    				+ "+ spend number in 3 digits ex)1001,2033");
+	    		System.out.print("Type of Money Spend:");
+	    		int moneytype = input.nextInt();
+	    		this.setMoneytype(moneytype);
+	    		
+	    		System.out.print("Date of Money Spend History:");
+	    		String moneydate = input.next();
+	    		this.setMoneydate(moneydate);
+	    		
+	    		System.out.print("Amount of Money Spend History(Won):");
+	    		int moneyamount = input.nextInt();
+	    		this.setMoneyamount(moneyamount);
+	    		
+	    		System.out.print("Detail of Money Spend History(No Space):");
+	    		input.nextLine();
+	    		String moneydetail = input.nextLine();
+	    		this.setMoneydetail(moneydetail);
 	            break;
 	        }
 	        
